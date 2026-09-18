d183c6b39c7df66e16d246d6fb5850f296772a3e
nikkolas-cage <nikkolascage01@gmail.com>
2026-09-18 18:05:13 +0800

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/post", (req, res) => {
+  const { name, value } = req.body;
+
+  console.log("Received post:", { name, value });
+
+  if (name === "foo" && value === "bar") {
+    return res.json(true);
+  }
+
+  res.send("no you didnt fo barred me");
+});
+
+app.get("/", (_req, res) => {
+  res.send("POST to /post with JSON { \"name\": \"foo\", \"value\": \"bar\" }");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
